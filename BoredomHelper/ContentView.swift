d06f72d125c52a2588f4eb5b3b8e420e497421e1
Francis Breidenbach <fbreidenbach@gmail.com>
2019-10-28 00:31:38 -0700

    initial commit

---
+++ BoredomHelper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoredomHelper
+//
+//  Created by Francis Breidenbach on 10/28/19.
+//  Copyright © 2019 Francis Breidenbach. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
